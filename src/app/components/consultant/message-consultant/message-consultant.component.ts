e8f0537e3bfb72418baf6efcbca5f8816bbe6cba
Your Name <you@example.com>
2020-12-14 14:50:29 +0700

    vinh_mt 14/12/2020 làm chức năng message

---
+++ src/app/components/consultant/message-consultant/message-consultant.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {MessageService} from '../../../service/message.service';
+
+@Component({
+  selector: 'app-message-consultant',
+  templateUrl: './message-consultant.component.html',
+  styleUrls: ['./message-consultant.component.css']
+})
+export class MessageConsultantComponent implements OnInit {
+
+  listMessage = [];
+
+  sub: Subscription;
+
+  constructor(private activatedRoute: ActivatedRoute, private messageService: MessageService) {
+  }
+
+  ngOnInit() {
+    this.sub = this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      const room = paramMap.get('id');
+      this.messageService.getMess(room).subscribe(data => {
+        this.listMessage = data;
+      });
+    });
+  }
+
+}
