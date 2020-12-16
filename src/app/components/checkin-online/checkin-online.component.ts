cde1104f57aacf2d9f662d3cdcab312e164c477f
Your Name <khanh06111993@gmail.com>
2020-12-16 14:38:57 +0700

    16/12

---
+++ src/app/components/checkin-online/checkin-online.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkin-online',
+  templateUrl: './checkin-online.component.html',
+  styleUrls: ['./checkin-online.component.css']
+})
+export class CheckinOnlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
