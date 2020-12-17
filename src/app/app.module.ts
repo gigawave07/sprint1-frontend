3f6a17145f01fdce084cd8b84616837de20900ea
gigawave07 <gigawave07@gmail.com>
2020-12-17 23:43:20 +0700

    quan_hm login with google, no data received

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,12 @@ import {Ng2SearchPipeModule} from 'ng2-search-filter';
 import {AdminComponent} from './components/admin/admin.component';
 import {InformationAdminComponent} from './components/information-admin/information-admin.component';
 import {ChangePasswordAdminComponent} from './components/change-password-admin/change-password-admin.component';
+import {
+  AuthServiceConfig,
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  SocialLoginModule
+} from "ngx-angular-social-login";
 
 @NgModule({
   declarations: [
@@ -33,14 +39,35 @@ import {ChangePasswordAdminComponent} from './components/change-password-admin/c
     AdminComponent, InformationAdminComponent, ChangePasswordAdminComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, MaterialModule, HttpClientModule, NgxPaginationModule, Ng2SearchPipeModule,
-    FormsModule, MatDialogModule, ReactiveFormsModule, NgxLoadingModule.forRoot({})],
+    FormsModule, MatDialogModule, ReactiveFormsModule, NgxLoadingModule.forRoot({}),
+    SocialLoginModule,
+
+  ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptor,
     multi: true
+  },  {
+    provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs
   }],
   bootstrap: [AppComponent],
   entryComponents: [SpinnerOverlayComponent]
 })
 export class AppModule {
+}
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+    [
+      {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider("927298648089577")
+      },
+      {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider("462543400761-9dlslrfotn225t12crirhc7hpcmdemcu.apps.googleusercontent.com")
+      },
+    ]
+  )
+  return config;
 }
