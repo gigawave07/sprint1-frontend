1f301b377fa8bb0791373eb030963fb30bac875d
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2020-12-17 09:36:25 +0700

    dang_nh 17/12

--- src/app/components/list-pending-ticket/cancel-pending-ticket/cancel-pending-ticket.component.ts
+++ src/app/components/list-pending-ticket/cancel-pending-ticket/cancel-pending-ticket.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {TicketService} from '../../../service/ticket/ticket.service';
+import {TicketStatusPaymentDTO} from '../../../model/TicketStatusPaymentDTO';
 
 @Component({
   selector: 'app-cancel-pending-ticket',
@@ -9,6 +10,7 @@ import {TicketService} from '../../../service/ticket/ticket.service';
 })
 export class CancelPendingTicketComponent implements OnInit {
   public ticketCancel;
+  public ticketStatusPaymentDTO = {id: 1, statusPaymentName: ''};
 
   constructor(
       public dialogRef: MatDialogRef<CancelPendingTicketComponent>,
@@ -18,11 +20,16 @@ export class CancelPendingTicketComponent implements OnInit {
 
   ngOnInit() {
     this.ticketCancel = this.data.dataCancel;
+    console.log('this.ticketCancel');
     console.log(this.ticketCancel);
   }
 
   cancelTicket() {
-    this.ticketService.cancelPendingTicket(this.ticketCancel.id, this.ticketCancel).subscribe(value => {
+    this.ticketStatusPaymentDTO.id = this.ticketCancel.id;
+    this.ticketStatusPaymentDTO.statusPaymentName = 'Cancel';
+    console.log('this.ticketStatusPaymentDTO');
+    console.log(this.ticketStatusPaymentDTO);
+    this.ticketService.setTicketStatusPayment(this.ticketStatusPaymentDTO.id, this.ticketStatusPaymentDTO).subscribe(value => {
       this.dialogRef.close();
     });
   }
--- src/app/components/search-pending-ticket/search-pending-ticket/search-pending-ticket.component.ts
+++ src/app/components/search-pending-ticket/search-pending-ticket/search-pending-ticket.component.ts
@@ -1,26 +1,99 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {TicketService} from "../../../service/ticket/ticket.service";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {TicketService} from '../../../service/ticket/ticket.service';
+import {MatDialog} from '@angular/material/dialog';
+import {CancelPendingTicketComponent} from '../../list-pending-ticket/cancel-pending-ticket/cancel-pending-ticket.component';
 
 @Component({
-  selector: 'app-search-pending-ticket',
-  templateUrl: './search-pending-ticket.component.html',
-  styleUrls: ['./search-pending-ticket.component.css']
+	selector: 'app-search-pending-ticket',
+	templateUrl: './search-pending-ticket.component.html',
+	styleUrls: ['./search-pending-ticket.component.css']
 })
 export class SearchPendingTicketComponent implements OnInit {
-  public formEmployee: FormGroup;
+	public ticketList = [];
+	public payTicketList = [];
+	public p = 1;
+	public isChecked: boolean;
+	public formTicketSearchDTO: FormGroup;
+	public searchByValue = null;
+	public message1: any;
 
-  constructor(
-      public formBuilder: FormBuilder,
-      // public dialogRef: MatDialogRef<EmployeeAddComponent>,
-      private ticketService: TicketService
-      // @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+	constructor(
+		private ticketService: TicketService,
+		private dialog: MatDialog,
+		public formBuilder: FormBuilder,
+	) {
+	}
 
-  ngOnInit() {
-    // this.formEmployee = this.formBuilder.group({
-    //
-    // });
-  }
+	ngOnInit(): void {
+		this.formTicketSearchDTO = this.formBuilder.group({
+			statusPaymentName: ['Pending', [Validators.required]],
+			searchBy: ['', [Validators.required]],
+			searchValue: ['', [Validators.required]],
+		});
+		console.log(this.formTicketSearchDTO.value);
+		console.log('this.ticketList');
+		console.log(this.ticketList);
+		// this.ticketService.findAllPendingTicket().subscribe(value => {
+		//      this.ticketList = value;
+		//      // console.log('this.ticketList');
+		//      // console.log(this.ticketList);
+		//
+		//    });
+		// console.log(this.searchByValue);
 
+
+	}
+
+	onCheckboxChange($event: Event, ticket: any) {
+		// @ts-ignore
+		this.isChecked = $event.target.checked;
+		console.log(ticket.ticketCode + ' ' + this.isChecked);
+		if (this.isChecked) {
+			this.payTicketList.push(ticket);
+			console.log(this.payTicketList);
+
+		} else {
+			console.log('ticket');
+			console.log(ticket);
+			console.log('ticket.id');
+			console.log(ticket.id);
+			this.payTicketList = this.payTicketList.filter(value => value !== ticket);
+			console.log(this.payTicketList);
+		}
+	}
+
+
+	openCancelDialogTicket(ticket: any): void {
+		const dialogRef = this.dialog.open(CancelPendingTicketComponent, {
+			width: '500px',
+			data: {dataCancel: ticket},
+			disableClose: true
+		});
+
+		dialogRef.afterClosed().subscribe(result => {
+			// this.ngOnInit();
+			this.ticketService.searchTicket(this.formTicketSearchDTO.value).subscribe(data => {
+				this.ticketList = data;
+				console.log(this.ticketList);
+			}, error => {
+				this.ticketList = [];
+				console.log('error searchTicket');
+			});
+		});
+	}
+
+	searchTicket() {
+		console.log(this.formTicketSearchDTO.value);
+		// this.ticketSearchDTO = this.formTicketSearchDTO.value;
+		// console.log('this.ticketSearchDTO abc');
+		// console.log(this.ticketSearchDTO);
+		this.ticketService.searchTicket(this.formTicketSearchDTO.value).subscribe(data => {
+			this.ticketList = data;
+			console.log(this.ticketList);
+		}, error => {
+			this.ticketList = [];
+			console.log('error searchTicket');
+		});
+	}
 }
