2eb3294bb771b06767b912a520689b363e721238
Gigawave07 <gigawave07@gmail.com>
2020-12-20 21:39:45 +0700

    add dd/mm/yyyy format global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -97,7 +97,7 @@ import {PromotionModule} from './components/promotion/promotion.module';
   }, {
     provide: AuthServiceConfig,
     useFactory: getAuthServiceConfigs
-  },
+  }, { provide: LOCALE_ID, useValue: 'pt-VN' },
     ExcelService],
   bootstrap: [AppComponent],
   entryComponents: [SpinnerOverlayComponent, TrangChinhComponent, BaoCaoComponentComponent, TrangChuThongKeComponent,
